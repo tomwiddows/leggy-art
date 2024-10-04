9613903433a472d7d8da5548d4dd3ee6ae94340b
Thomas Widdows <thomas.widdows10@gmail.com>
2024-10-04 17:53:56 +0000

    Add admin classes for each model

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Print, Category, Review
 
 # Register your models here.
+class PrintAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'orientation',
+        'rating',
+        'image_1_path',
+        'image_1',
+        'image_2_path',
+        'image_2',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'user',
+        'content',
+        'rating',
+        'created_at',
+    )
+
+
+admin.site.register(Print, PrintAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Review, ReviewAdmin)
