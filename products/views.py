4e59a99cd65cac3317d148542a4f7be2b4628d34
Thomas Widdows <thomas.widdows10@gmail.com>
2024-10-08 22:14:00 +0000

    Add search functionality

--- products/views.py
+++ products/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from django.http import HttpResponseRedirect
+from urllib.parse import urlencode
 from .models import Print
 
 # Create your views here.
@@ -7,9 +11,30 @@ def all_prints(request):
     """ A view to show individual product details """
 
     products = Print.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                
+                get_params = request.GET.copy()
+                get_params.pop('q', None)
+                
+                # Construct new URL without 'q' parameter
+                path = request.path
+                if get_params:
+                    path += '?' + urlencode(get_params)
+                
+                return HttpResponseRedirect(path)
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/prints.html', context)
