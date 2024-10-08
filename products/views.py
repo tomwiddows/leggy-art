80366149e46a45d696b4d4c22e3f0534ed265bef
Thomas Widdows <thomas.widdows10@gmail.com>
2024-10-08 21:26:41 +0000

    Remove s from products

--- products/views.py
+++ products/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Print
 
 # Create your views here.
 
 def all_prints(request):
-    """ A view to show all prints, including sorting and search queries """
+    """ A view to show individual product details """
 
     products = Print.objects.all()
 
@@ -13,3 +13,15 @@ def all_prints(request):
     }
 
     return render(request, 'products/prints.html', context)
+
+
+def print_detail(request, print_id):
+    """ A view to show all prints, including sorting and search queries """
+
+    product = get_object_or_404(Print, pk=print_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/print_detail.html', context)
