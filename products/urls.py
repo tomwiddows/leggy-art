7e7f24afa119c74e661d1b4a161b0993d61a3589
Thomas Widdows <thomas.widdows10@gmail.com>
2024-10-08 21:26:10 +0000

    nothing changed. Bug fixing trial & error

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_prints, name='products')
+    path('', views.all_prints, name='products'),
+    path('<print_id>', views.print_detail, name='product_detail'),
 ]
