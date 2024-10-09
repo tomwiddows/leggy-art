94de2483a693753d6ef94fe1a16ff7d1b42df22d
Thomas Widdows <thomas.widdows10@gmail.com>
2024-10-09 21:00:40 +0000

    Add remove item functionality

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -23,3 +23,39 @@ def add_to_basket(request, item_id):
     request.session['basket'] = basket
 
     return redirect(redirect_url)
+
+
+def view_basket(request):
+    """ A view to renders the basket contents page """
+
+    return render(request, 'basket/basket.html')
+
+
+def adjust_basket(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop[item_id]
+
+    request.session['basket'] = basket
+
+    return redirect(reverse('view_basket'))
+
+
+def remove_from_basket(request, item_id):
+    """ Remove the item from the shopping bag """
+    try:
+        basket = request.session.get('basket', {})
+
+        basket.pop(item_id)
+
+        request.session['basket'] = basket
+
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
