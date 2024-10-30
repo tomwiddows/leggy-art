0b68d751f574a26ac0ac201a19c9eb466e7202b3
Thomas Widdows <thomas.widdows10@gmail.com>
2024-10-30 13:51:15 +0000

    Add cache control

--- leggy_art/settings.py
+++ leggy_art/settings.py
@@ -159,6 +159,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 print(os.environ.get('USE_AWS'))
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+        'ContentType': 'application/octet-stream',
+    }
     print("AWS settings activated")
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'leggy-art'
