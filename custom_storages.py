d75d658db396818415e323abc4e0fcaf73488783
Thomas Widdows <thomas.widdows10@gmail.com>
2024-10-30 17:47:36 +0000

    Attempt to upload static files directly to S3

--- custom_storages.py
+++ custom_storages.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
+import boto3
+import os
 
 
 class StaticStorage(S3Boto3Storage):
@@ -8,3 +10,10 @@ class StaticStorage(S3Boto3Storage):
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
+
+s3 = boto3.client('s3', 
+                  aws_access_key_id=os.environ.get('AWS_ACCES_KEY_ID'), 
+                  aws_secret_access_keys=os.environ.get('AWS_SECRET_ACCES_KEY')
+                )
+
+s3.put_object(Bucket='your-bucket-name', Key='test.txt', Body='Hello, world!')
