2263d839d20c8071aab76d86daf717b12e63bd47
Thomas Widdows <thomas.widdows10@gmail.com>
2024-10-30 16:13:39 +0000

    Alter static and media urls

--- leggy_art/settings.py
+++ leggy_art/settings.py
@@ -178,8 +178,8 @@ if 'USE_AWS' in os.environ:
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}.s3.amazonaws.com/static/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}.s3.amazonaws.com/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
