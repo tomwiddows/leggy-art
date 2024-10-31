18ef620ede2c5772d959d1b5da6f4a4a0e350944
Thomas Widdows <thomas.widdows10@gmail.com>
2024-10-31 14:08:42 +0000

    Alter value of STATICFILES_STORAGE and DEFAULT_FILE_STORAGE

--- leggy_art/settings.py
+++ leggy_art/settings.py
@@ -173,10 +173,10 @@ if 'USE_AWS' in os.environ:
     print(AWS_S3_CUSTOM_DOMAIN)
 
     # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_STORAGE = custom_storages.StaticStorage
     print(STATICFILES_STORAGE)
     STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    DEFAULT_FILE_STORAGE = custom_storages.MediaStorage
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
