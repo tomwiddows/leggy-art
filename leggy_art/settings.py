c823f465eec0fecb838808dd2c359a8668752cf0
Thomas Widdows <thomas.widdows10@gmail.com>
2024-10-31 15:34:08 +0000

    Change order of staticfiles_storage & location, as well as default_file_storage and mediafiles_location

--- leggy_art/settings.py
+++ leggy_art/settings.py
@@ -174,10 +174,10 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
-    STATICFILES_LOCATION = 'static'
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    MEDIAFILES_LOCATION = 'media'
+    STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
