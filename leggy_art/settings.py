5403101b7076eb249e730c89e8462976fb216e6e
Thomas Widdows <thomas.widdows10@gmail.com>
2024-11-05 00:43:36 +0000

    Alter email settings to use SSL instead of TLS

--- leggy_art/settings.py
+++ leggy_art/settings.py
@@ -236,12 +236,12 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
-if 'DEVELOPMENT' in os.environ:
+if os.environ.get('DEVELOPMENT').lower() == 'true':
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'leggyartshop@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
+    EMAIL_USE_SSL = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
