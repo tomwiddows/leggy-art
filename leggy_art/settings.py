6d4af59b68f13d9d339dd58c1c01fa2b759addae
Thomas Widdows <thomas.widdows10@gmail.com>
2024-11-08 03:05:29 +0000

    USE_AWS if statement for cache control

--- leggy_art/settings.py
+++ leggy_art/settings.py
@@ -174,10 +174,12 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Cache control
-AWS_S3_OBJECT_PARAMETERS = {
-    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-    'CacheControl': 'max-age=94608000',
-}
+if USE_AWS in os.environ:
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
 # Bucket config
 AWS_STORAGE_BUCKET_NAME = 'leggy-art'
 AWS_S3_REGION_NAME = 'eu-west-2'
