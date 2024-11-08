2f28e9f7b434d2e3466c4e600faa5653ae1010bb
Thomas Widdows <thomas.widdows10@gmail.com>
2024-11-08 04:52:54 +0000

    Remove USE_AWS if statement

--- leggy_art/settings.py
+++ leggy_art/settings.py
@@ -164,11 +164,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Cache control settings for AWS
-if USE_AWS in os.environ:
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
-    }
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000',
+}
 
 # AWS S3 configuration
 AWS_STORAGE_BUCKET_NAME = 'leggy-art'
